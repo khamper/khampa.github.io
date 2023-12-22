d0e4bb4de9a0e7e068170df6fdb68507eca61e44
khamper <148259533+khamper@users.noreply.github.com>
2023-12-22 18:51:08 +0200

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,6 +1,21 @@
 let message = "Servers are unreachable.";
+let currentUser;
+
+var prvr = 0;
+
+function connectToServer()
+{
+    currentUser="debug";//for now
+    console.log("logged in as:", currentUser);
+    // TODO: add proper code here
+}
 
 function popup()
 {
-    alert(message);
+    if(prvr===1)
+    {
+        connectToServer();
+    } else{
+        alert(message);
+    }
 }
