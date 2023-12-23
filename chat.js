f211d5226cb1d5b8b33946ce174aef83e1ed8ab4
khamper <148259533+khamper@users.noreply.github.com>
2023-12-23 19:30:43 +0200

    make enter chat room open chatroom.html window

--- chat.js
+++ chat.js
@@ -1,26 +1,26 @@
 //*trying to do this without any tutorials*
 //i forbid the usage of any tutorials to make this
 //currently very simple and very useless
+
 let currentUser;
 
 function getUsername()
 {
-    //try to ask for username and then print to console the current username
     try{
         currentUser = prompt("enter your username:");
     }finally{
         console.log("logged in as:", currentUser);
     }
-}
-function popup()
-{
-    getUsername();
+    openChatRoom();
 }
 function openChatRoom() 
 {
     if(currentUser!=null && currentUser!="")
-        window.open("index.html","Chat Room","width=300,height=200");  
+    {
+        window.open("chatroom.html","Chat Room","width=300,height=200").innerHTML= "o priviec, " + currentUser;
+        // document.getElementById("testas").innerHTML= "o priviec, " + currentUser;
+    }
     else{
-        popup();
+        getUsername();
     }
 }
