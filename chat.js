0dfdc5b5d8fe9d70da8916b940d34fe9332eb1b6
khamper <148259533+khamper@users.noreply.github.com>
2023-12-27 00:52:45 +0200

    minor bug fixes and slightly improved chatroom login system

--- chat.js
+++ chat.js
@@ -3,21 +3,28 @@
 //currently very simple and very useless
 
 let currentUser;
+
 function login() 
 {
-    try{
-        while(currentUser==null || currentUser=="")
-            currentUser = prompt("enter your username:");
-    }finally{
-        console.log("logged in as:", currentUser);
-        alert("you may now enter the chat room");
+    if(currentUser!=null) 
+    {
+        alert("you have already logged in");
+    }
+    else{
+        try{
+            while(currentUser==null || currentUser=="")
+                currentUser = prompt("enter your username:");
+        }finally{
+            console.log("logged in as:", currentUser);
+            alert("you may now enter the chat room");
+        }
     }
 }
 function enterChatRoom()
 {
     if(currentUser!=null)
     {
-        window.open("chatroom.html","Chat Room","width=300,height=200").innerHTML= "o priviec, " + currentUser;
+        window.open("chatroom.html","Chat Room","width=","height=");
     }
     else{
         alert("you must log in first");
